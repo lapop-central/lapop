cc2882e639bb61ad8b992e3af9f0aee0d4e7620e
Robert Vidigal <robert.vidigal@vanderbilt.edu>
2025-05-13 15:18:27 -0500

    Update lpr_dumb.R

--- R/lpr_dumb.R
+++ R/lpr_dumb.R
@@ -68,7 +68,7 @@
 #'
 #'@export
 #'@import dplyr
-#'@Import tidyr
+#'@import tidyr
 #'@import srvyr
 #'
 #'@author Luke Plutowski, \email{luke.plutowski@@vanderbilt.edu} && Robert Vidigal, \email{robert.vidigal@@vanderbilt.edu}
@@ -105,11 +105,11 @@ lpr_dumb <- function(data,
         group_by(wave1 = as.character(as_factor(wave))) %>%
         {
           if (mean) {
-            summarize(.,
+            summarize(.data,
                       prop1 = survey_mean(!!sym(out), na.rm = TRUE, vartype = "ci", level = ci_level)) %>%
               mutate(proplabel1 = if (cfmt != "") sprintf(cfmt, prop1) else sprintf("%.1f", prop1))
           } else {
-            summarize(.,
+            summarize(.data,
                       prop1 = survey_mean(between(!!sym(out), rec[1], rec[2]), na.rm = TRUE,
                                           vartype = "ci", level = ci_level) * 100) %>%
               mutate(proplabel1 = if (cfmt != "") sprintf(cfmt, round(prop1)) else sprintf("%.0f%%", round(prop1)))
@@ -122,11 +122,11 @@ lpr_dumb <- function(data,
         group_by(wave2 = as.character(as_factor(wave))) %>%
         {
           if (mean) {
-            summarize(.,
+            summarize(.data,
                       prop2 = survey_mean(!!sym(out), na.rm = TRUE, vartype = "ci", level = ci_level)) %>%
               mutate(proplabel2 = if (cfmt != "") sprintf(cfmt, prop2) else sprintf("%.1f", prop2))
           } else {
-            summarize(.,
+            summarize(.data,
                       prop2 = survey_mean(between(!!sym(out), rec[1], rec[2]), na.rm = TRUE,
                                           vartype = "ci", level = ci_level) * 100) %>%
               mutate(proplabel2 = if (cfmt != "") sprintf(cfmt, round(prop2)) else sprintf("%.0f%%", round(prop2)))
@@ -193,44 +193,44 @@ lpr_dumb <- function(data,
     dumb <- merge(wave1, wave2, by = "pais")
   }
 
-  dumb = dumb %>%
-    {
-      if (sort == "prop1") {
-        if (order == "hi-lo") {
-          arrange(., desc(prop1))
-        } else if (order == "lo-hi") {
-          arrange(., prop1)
-        }
-      } else if (sort == "prop2") {
-        if (order == "hi-lo") {
-          arrange(., desc(prop2))
-        } else if (order == "lo-hi") {
-          arrange(., prop2)
-        }
-      } else if (sort == "xv") {
-        if (order == "hi-lo") {
-          arrange(., desc(match(pais, levels(pais))))
-        } else if (order == "lo-hi") {
-          arrange(., match(pais, levels(pais)))
-        }
-      } else if (sort == "diff") {
-        if (order == "hi-lo") {
-          mutate(., diff = prop2 - prop1) %>%
-            arrange(., desc(diff))
-        } else if (order == "lo-hi") {
-          mutate(., diff = prop2 - prop1) %>%
-            arrange(., diff)
-        }
-      } else if (sort == "xl") {
-        if (order == "hi-lo") {
-          arrange(., desc(as.character(xvar)))
-        } else if (order == "lo-hi") {
-          arrange(., as.character(xvar))
-        } else {
-          .  # Return unchanged if no valid sorting option is selected
-        }
+ dumb = dumb %>%
+  {
+    if (sort == "prop1") {
+      if (order == "hi-lo") {
+        arrange(.data, desc(prop1))
+      } else if (order == "lo-hi") {
+        arrange(.data, prop1)
+      }
+    } else if (sort == "prop2") {
+      if (order == "hi-lo") {
+        arrange(.data, desc(prop2))
+      } else if (order == "lo-hi") {
+        arrange(.data, prop2)
+      }
+    } else if (sort == "xv") {
+      if (order == "hi-lo") {
+        arrange(.data, desc(match(pais, levels(pais))))
+      } else if (order == "lo-hi") {
+        arrange(.data, match(pais, levels(pais)))
+      }
+    } else if (sort == "diff") {
+      if (order == "hi-lo") {
+        mutate(.data, diff = prop2 - prop1) %>%
+          arrange(.data, desc(diff))
+      } else if (order == "lo-hi") {
+        mutate(.data, diff = prop2 - prop1) %>%
+          arrange(.data, diff)
+      }
+    } else if (sort == "xl") {
+      if (order == "hi-lo") {
+        arrange(.data, desc(as.character(xvar)))
+      } else if (order == "lo-hi") {
+        arrange(.data, as.character(xvar))
+      } else {
+        .data  # Return unchanged
       }
     }
+  }
 
 
   if (ttest) {
