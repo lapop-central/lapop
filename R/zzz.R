d2221d8d586f2bf07dfb08f4b7f0c7d3d673e829
Robert Vidigal <robert.vidigal@vanderbilt.edu>
2025-05-15 12:00:54 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,8 @@
 # Package startup message
 lapopWelcomeMessage <- function(pkg){   
    paste("\n",     
-    "LAPOP Lab R package. Version:", utils::packageVersion(pkg), "loaded successfully\n",
-    "For documentation, run: browseVignettes('lapop') or ?lapop", "\n",
+    "LAPOP Lab R package. Version: ", utils::packageVersion(pkg), " loaded successfully\n",
+    #"For documentation, run: browseVignettes('lapop') or ?lapop", "\n",
     "For Americasbarometer datasets, please visit https://www.vanderbilt.edu/lapop/", "\n", 
     "To report issues or suggestions, please visit: https://github.com/lapop-central/lapop/issues", "\n",               
     "Contact: <robert.vidigal@vanderbilt.edu>\n", sep="")
