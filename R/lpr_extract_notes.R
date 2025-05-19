eae8b52180e1c066a75d2ed65dec7847e8ea0e33
Robert Vidigal <robert.vidigal@vanderbilt.edu>
2025-05-19 17:16:24 -0500

    Update lpr_extract_notes.R

--- R/lpr_extract_notes.R
+++ R/lpr_extract_notes.R
@@ -30,22 +30,23 @@
 #'
 #' @examples
 #' # Extract the notes
+#' \dontrun{
 #' notes <- lpr_extract_notes(df)
-#' print(notes)
+#' print(notes)}
 #'
 #' @export
 
 lpr_extract_notes <- function(data) {
   notes_df <- data.frame(variable_name = character(), noteid = character(), note_value = character(), stringsAsFactors = FALSE)
-  
+
   for (i in seq_along(data)) {
     sublist <- data[[i]]
-    
+
     if (length(sublist) == 3) {
       variable_name <- sublist[[1]]
       noteid <- sublist[[2]]
       note_value <- sublist[[3]]
-      
+
        if (variable_name!="_dta") {
         # Add the row to notes_df
         notes_df <- rbind(notes_df, data.frame(variable_name = variable_name, note_id = noteid, note_value = note_value, stringsAsFactors = FALSE))
