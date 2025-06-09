9475030bf937589c13af0fa374cfa02711fe7ea1
Robert Vidigal <robert.vidigal@vanderbilt.edu>
2025-06-09 16:41:57 -0500

    update bra23

---
+++ R/bra23.R
@@ -0,0 +1,24 @@
+#' bra23 Dataset
+#'
+#' A dataset containing the AmericasBarometer Brazil 2023.
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{aoj11}{Description of Column}
+#'   \item{ing4}{Description of Column}
+#'   \item{b13}{Description of Column}
+#'   \item{b21}{Description of Column}
+#'   \item{b31}{Description of Column}
+#'   \item{b12}{Description of Column}
+#'   \item{wave}{Description of Column}
+#'   \item{pais}{Description of Column}
+#'   \item{year}{Description of Column}
+#'   \item{upm}{Description of Column}
+#'   \item{strata}{Description of Column}
+#'   \item{weight1500}{Description of Column}
+#'   \item{wt}{Description of Column}
+#'   ...
+#' }
+#' @source LAPOP AmericasBarometer (https://www.vanderbilt.edu/lapop/)
+#' @export
+bra23 <- load("data/bra23.rda")
