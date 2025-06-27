befce52f324fc18f61dacc866a346a98395f0e39
Robert Vidigal <robert.vidigal@vanderbilt.edu>
2025-06-26 22:20:56 -0500

    ym23 fix

--- R/ym23.R
+++ R/ym23.R
@@ -4,24 +4,25 @@
 #'
 #' @format A data frame
 #' \describe{
-#'   \item{aoj11}{Description of Column}
-#'   \item{ing4}{Description of Column}
-#'   \item{b13}{Description of Column}
-#'   \item{b21}{Description of Column}
-#'   \item{b31}{Description of Column}
-#'   \item{b12}{Description of Column}
-#'   \item{wave}{Description of Column}
-#'   \item{pais}{Description of Column}
-#'   \item{year}{Description of Column}
-#'   \item{b18}{Description of Column}
-#'   \item{pn4}{Description of Column}
-#'   \item{edre}{Description of Column}
-#'   \item{wealth}{Description of Column}
-#'   \item{q1tc_r}{Description of Column}
-#'   \item{vb21n}{Description of Column}
-#'   \item{q14f}{Description of Column}
-#'   \item{strata}{Description of Column}
-#'   \item{weight1500}{Description of Column}
+#' \item{aoj11}{Description of Column}
+#' \item{ing4}{Description of Column}
+#' \item{b13}{Description of Column}
+#' \item{b21}{Description of Column}
+#' \item{b31}{Description of Column}
+#' \item{b12}{Description of Column}
+#' \item{wave}{Description of Column}
+#' \item{pais}{Description of Column}
+#' \item{year}{Description of Column}
+#' \item{b18}{Description of Column}
+#' \item{pn4}{Description of Column}
+#' \item{edre}{Description of Column}
+#' \item{wealth}{Description of Column}
+#' \item{q1tc_r}{Description of Column}
+#' \item{vb21n}{Description of Column}
+#' \item{q14f}{Description of Column}
+#' \item{upm}{Description of Column}
+#' \item{strata}{Description of Column}
+#' \item{weight1500}{Description of Column}
 #' }
 #' @source LAPOP AmericasBarometer (https://www.vanderbilt.edu/lapop/)  # Ensure this line has a valid source.
 #' @export
